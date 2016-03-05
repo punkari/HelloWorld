3a52206912d68de7e8578e17d2c41a91d0e111a9
punkari <gitpunkari@nym.hush.com>
2016-03-05 18:56:28 +0000

    Create source.c

---
+++ source.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+        puts("fin: Terve maailma.");
+        puts("swe: Hej varlden.");
+        puts("is: Hae heimur.");
+        puts("eng: Hello, World.");
+}
